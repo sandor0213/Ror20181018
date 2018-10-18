82d2c256d7d49dc4fb939639bee9dc85d5acaf6d
sandor0213 <19950213>
2018-10-18 23:28:25 +0300

    Second

---
+++ db/migrate/20181018073347_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :age
+      t.text :desc
+
+      t.timestamps
+    end
+  end
+end
